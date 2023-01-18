4a5984bd4a3a0b707408463bce82be43e62bbb93
MariusAdrianStoica <stoichitza@yahoo.com>
2023-01-18 15:50:22 +0100

    initial commit

---
+++ src/main/java/se/lexicon/ms_lecture_jpa/MsLectureJpaApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.ms_lecture_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLectureJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsLectureJpaApplication.class, args);
+    }
+
+}
